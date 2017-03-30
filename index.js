c3860ef5039c42863d5aaccf51d5a093b9a3cfe3
lukem512 <hi@lukemitchell.co>
2017-03-30 13:31:18 +0100

    Update dependencies, module

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 import React from 'react'
-import parse from 'parse-svg-path'
-import extract from 'extract-svg-path'
+const viewBox = require('extract-svg-viewbox')
+const normalize = require('normalize-svg-coords')
+const extract = require('extract-svg-path').parse
 
 const SvgPartialFill = React.createClass({
   propTypes: {
-    src: React.PropTypes.string.isRequired,
+    svg: React.PropTypes.string.isRequired,
     width: React.PropTypes.number,
     height: React.PropTypes.number,
     background: React.PropTypes.string,
     fill: React.PropTypes.string,
-    percent: React.PropTypes.number
+    percent: React.PropTypes.number,
+    style: React.PropTypes.object
   },
 
-  componentWillMount() {
-    this.loadSvg(this.props.src)
-  },
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.src !== nextProps.src) {
-      this.loadSvg(nextProps.src)
+  getInitialState() {
+    return {
+      ready: false,
+      rid: Date.now()
     }
   },
 
-  // TODO: add a random id to svgPath
+  componentWillMount() {
+    this.extractPaths(this.props.svg)
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.svg !== nextProps.svg) {
+      this.extractPaths(nextProps.svg)
+    }
+  },
+
   renderDefs() {
     return (<svg width="0px" height="0px">
       <defs>
-        <clipPath id="svgPath" clipPathUnits="objectBoundingBox">
+        <clipPath id={`svgPath-${this.state.rid}`}
+          clipPathUnits="objectBoundingBox">
           <path d={this.state.pathData} />
         </clipPath>
       </defs>
@@ -37,21 +46,23 @@ const SvgPartialFill = React.createClass({
     const {
       width = '50px',
       height = '50px',
+      fill = 'lightskyblue',
+      fillTo = 'transparent',
+      percent = '50',
       background: backgroundColor = 'white',
-      fill = 'red',
-      percent = '33'
+      direction = 'to right'
     } = this.props;
 
-    // TODO: specify direction in props
     return (
-      <div class="svg-outer" style={{
+      <div className="svg-outer" style={{
+        ...this.props.style,
         backgroundColor,
         width,
         height
       }}>
-        <div class="svg-inner" style={{
-          background: `linear-gradient(to right, ${fill}, ${fill} ${percent}%, transparent ${percent}%)`,
-          clipPath: 'url(#svgPath)',
+        <div className="svg-inner" style={{
+          background: `linear-gradient(${direction}, ${fill}, ${fill} ${percent}%, ${fillTo} ${percent}%)`,
+          clipPath: `url(#svgPath-${this.state.rid})`,
           width: '100%',
           height: '100%'
         }}/>
@@ -61,19 +72,23 @@ const SvgPartialFill = React.createClass({
 
   render() {
     return (
-      <div>
-        {this.renderDefs()}
-        {this.renderSvg()}
-      </div>
+      <span>
+        {this.state.ready && this.renderDefs()}
+        {this.state.ready && this.renderSvg()}
+      </span>
     )
   },
 
-  loadSvg(src) {
-    const path = extract(src)
-    this.setState({
-      pathData: parse(path)
+  extractPaths(svg) {
+    const pathData = normalize({
+      viewBox: viewBox(svg),
+      path: extract(svg)
     })
-  }
+    this.setState({
+      pathData,
+      ready: true
+    })
+  },
 })
 
 export default SvgPartialFill
